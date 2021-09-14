fc29ba3f9493bcb18b71cb87e288837b98772354
tc-miyatani <miyahei38+tc@gmail.com>
2021-09-14 17:07:33 +0900

    タブ3アイコンの変更

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -20,6 +20,10 @@ import TabThreeScreen from '../screens/TabThreeScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
+import {
+  SearchIcon,
+  NativeBaseProvider,
+} from "native-base";
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -59,50 +63,52 @@ function BottomTabNavigator() {
   const colorScheme = useColorScheme();
 
   return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
-      <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="TabThree"
-        component={TabThreeScreen}
-        options={{
-          title: 'タブ3',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
+    <NativeBaseProvider>
+      <BottomTab.Navigator
+        initialRouteName="TabOne"
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme].tint,
+        }}>
+        <BottomTab.Screen
+          name="TabOne"
+          component={TabOneScreen}
+          options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
+            title: 'Tab One',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate('Modal')}
+                style={({ pressed }) => ({
+                  opacity: pressed ? 0.5 : 1,
+                })}>
+                <FontAwesome
+                  name="info-circle"
+                  size={25}
+                  color={Colors[colorScheme].text}
+                  style={{ marginRight: 15 }}
+                />
+              </Pressable>
+            ),
+          })}
+        />
+        <BottomTab.Screen
+          name="TabTwo"
+          component={TabTwoScreen}
+          options={{
+            title: 'Tab Two',
+            tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          }}
+        />
+        <BottomTab.Screen
+          name="TabThree"
+          component={TabThreeScreen}
+          options={{
+            title: 'タブ3',
+            tabBarIcon: ({ color }) => <SearchIcon color={color} />,
+          }}
+        />
+      </BottomTab.Navigator>
+    </NativeBaseProvider>
   );
 }
 
